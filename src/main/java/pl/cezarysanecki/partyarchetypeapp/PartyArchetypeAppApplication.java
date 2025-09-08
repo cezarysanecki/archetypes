ef1319e7cc6da92a668389412356f91abb6e4b7b
Cezary Sanecki <saneckicezary@gmail.com>
2025-09-08 08:29:33 +0200

    Init

---
+++ src/main/java/pl/cezarysanecki/partyarchetypeapp/PartyArchetypeAppApplication.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.partyarchetypeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyArchetypeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyArchetypeAppApplication.class, args);
+    }
+
+}
