11a3976d478a124234d0743b03b9b1f16eee3bed
Cezary Sanecki <saneckicezary@gmail.com>
2025-09-10 09:16:45 +0200

    Created two other use cases

---
+++ party-archetype-spring/src/main/java/pl/cezarysanecki/partyarchetypeapp/application/RegisterCompanyUseCase.java
@@ -0,0 +1,43 @@
+package pl.cezarysanecki.partyarchetypeapp.application;
+
+import org.springframework.stereotype.Component;
+import pl.cezarysanecki.partyarchetypeapp.model.OrganizationName;
+import pl.cezarysanecki.partyarchetypeapp.model.PartiesFacade;
+import pl.cezarysanecki.partyarchetypeapp.model.Role;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class RegisterCompanyUseCase {
+
+    private static final RegisteredIdentifiersFinder REGISTERED_IDENTIFIERS_FINDER = new RegisteredIdentifiersFinder();
+
+    private final PartiesFacade partiesFacade;
+
+    public RegisterCompanyUseCase(PartiesFacade partiesFacade) {
+        this.partiesFacade = partiesFacade;
+    }
+
+    public void registerCompany(RegisterCompanyCommand command) {
+        partiesFacade.registerCompanyFor(
+                new OrganizationName(command.name),
+                command.roles.stream().map(Role::new).collect(Collectors.toSet()),
+                command.registeredIdentifiers.stream()
+                        .map(identifier -> REGISTERED_IDENTIFIERS_FINDER.findBy(identifier.type(), identifier.number()))
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toSet())
+        );
+    }
+
+    public record RegisterCompanyCommand(String name,
+                                         Set<String> roles,
+                                         Set<RegisteredIdentifier> registeredIdentifiers) {
+        public record RegisteredIdentifier(
+                String type,
+                String number) {
+        }
+    }
+}
+
---
+++ party-archetype-spring/src/main/java/pl/cezarysanecki/partyarchetypeapp/application/RegisterOrganizationUnitUseCase.java
@@ -0,0 +1,42 @@
+package pl.cezarysanecki.partyarchetypeapp.application;
+
+import org.springframework.stereotype.Component;
+import pl.cezarysanecki.partyarchetypeapp.model.OrganizationName;
+import pl.cezarysanecki.partyarchetypeapp.model.PartiesFacade;
+import pl.cezarysanecki.partyarchetypeapp.model.Role;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class RegisterOrganizationUnitUseCase {
+
+    private static final RegisteredIdentifiersFinder REGISTERED_IDENTIFIERS_FINDER = new RegisteredIdentifiersFinder();
+
+    private final PartiesFacade partiesFacade;
+
+    public RegisterOrganizationUnitUseCase(PartiesFacade partiesFacade) {
+        this.partiesFacade = partiesFacade;
+    }
+
+    public void registerOrganizationUnit(RegisterOrganizationUnitCommand command) {
+        partiesFacade.registerOrganizationUnitFor(
+                new OrganizationName(command.name),
+                command.roles.stream().map(Role::new).collect(Collectors.toSet()),
+                command.registeredIdentifiers.stream()
+                        .map(identifier -> REGISTERED_IDENTIFIERS_FINDER.findBy(identifier.type(), identifier.number()))
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toSet())
+        );
+    }
+
+    public record RegisterOrganizationUnitCommand(String name,
+                                                  Set<String> roles,
+                                                  Set<RegisteredIdentifier> registeredIdentifiers) {
+        public record RegisteredIdentifier(
+                String type,
+                String number) {
+        }
+    }
+}
