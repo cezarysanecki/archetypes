3aab318a98602e34f0db876b50b3f6317b9866b8
Cezary Sanecki <cezary.sanecki@allegro.com>
2025-09-11 17:03:48 +0200

    Added tests for other parties

--- party-archetype-spring/src/test/java/pl/cezarysanecki/partyarchetypeapp/infrastructure/PartyStdTest.java
+++ party-archetype-spring/src/test/java/pl/cezarysanecki/partyarchetypeapp/infrastructure/PartyStdTest.java
@@ -3,6 +3,9 @@ package pl.cezarysanecki.partyarchetypeapp.infrastructure;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import pl.cezarysanecki.partyarchetypeapp.common.Version;
+import pl.cezarysanecki.partyarchetypeapp.model.Company;
+import pl.cezarysanecki.partyarchetypeapp.model.OrganizationName;
+import pl.cezarysanecki.partyarchetypeapp.model.OrganizationUnit;
 import pl.cezarysanecki.partyarchetypeapp.model.Party;
 import pl.cezarysanecki.partyarchetypeapp.model.PartyId;
 import pl.cezarysanecki.partyarchetypeapp.model.Person;
@@ -35,11 +38,58 @@ class PartyStdTest {
         Map<String, Object> values = OBJECT_MAPPER.convertValue(person, Map.class);
         Person result = (Person) OBJECT_MAPPER.convertValue(values, Party.class);
 
+        //then
         assertEquals(person.partyId(), result.partyId());
         assertEquals(person.personalData(), result.personalData());
         assertEquals(person.roles(), result.roles());
         assertEquals(person.registeredIdentifiers(), result.registeredIdentifiers());
         assertEquals(person.version(), result.version());
+    }
+
+    @Test
+    void checkIfCompanyPartyIsProperlySerializedAndDeserialized() {
+        //given
+        Company company = new Company(
+                PartyId.random(),
+                new OrganizationName("Pepsi Cola"),
+                Set.of(new Role("PAYER")),
+                Set.of(new SampleRegisteredIdentifier("12345678901")),
+                Version.initial()
+        );
+
+        //when
+        Map<String, Object> values = OBJECT_MAPPER.convertValue(company, Map.class);
+        Company result = (Company) OBJECT_MAPPER.convertValue(values, Party.class);
+
+        //then
+        assertEquals(company.partyId(), result.partyId());
+        assertEquals(company.organizationName(), result.organizationName());
+        assertEquals(company.roles(), result.roles());
+        assertEquals(company.registeredIdentifiers(), result.registeredIdentifiers());
+        assertEquals(company.version(), result.version());
+    }
+
+    @Test
+    void checkIfOrganizationUnitPartyIsProperlySerializedAndDeserialized() {
+        //given
+        OrganizationUnit organizationUnit = new OrganizationUnit(
+                PartyId.random(),
+                new OrganizationName("Pepsi Cola"),
+                Set.of(new Role("PAYER")),
+                Set.of(new SampleRegisteredIdentifier("12345678901")),
+                Version.initial()
+        );
+
+        //when
+        Map<String, Object> values = OBJECT_MAPPER.convertValue(organizationUnit, Map.class);
+        OrganizationUnit result = (OrganizationUnit) OBJECT_MAPPER.convertValue(values, Party.class);
+
+        //then
+        assertEquals(organizationUnit.partyId(), result.partyId());
+        assertEquals(organizationUnit.organizationName(), result.organizationName());
+        assertEquals(organizationUnit.roles(), result.roles());
+        assertEquals(organizationUnit.registeredIdentifiers(), result.registeredIdentifiers());
+        assertEquals(organizationUnit.version(), result.version());
     }
 
     record SampleRegisteredIdentifier(String value) implements RegisteredIdentifier {
